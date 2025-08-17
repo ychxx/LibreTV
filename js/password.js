63e5860fd1b34edb547aebc0b7a04ec5e35cf222
yc20522 <563919364@qq.com>
2025-08-17 19:13:23 +0800

    1

--- js/password.js
+++ js/password.js
@@ -45,66 +45,6 @@ window.isPasswordRequired = isPasswordRequired;
  */
 async function verifyPassword(password) {
     try {
-        if (password == "0930") {
-            API_SITES = API_SITES & {
-                ckzy: {
-                    api: 'https://www.ckzy1.com',
-                    name: 'CK资源',
-                    adult: true
-                },
-                jkun: {
-                    api: 'https://jkunzyapi.com',
-                    name: 'jkun资源',
-                    adult: true
-                },
-                bwzy: {
-                    api: 'https://api.bwzym3u8.com',
-                    name: '百万资源',
-                    adult: true
-                },
-                souav: {
-                    api: 'https://api.souavzy.vip',
-                    name: 'souav资源',
-                    adult: true
-                },
-                r155: {
-                    api: 'https://155api.com',
-                    name: '155资源',
-                    adult: true
-                },
-                lsb: {
-                    api: 'https://apilsbzy1.com',
-                    name: 'lsb资源',
-                    adult: true
-                },
-                huangcang: {
-                    api: 'https://hsckzy.vip',
-                    name: '黄色仓库',
-                    adult: true,
-                    detail: 'https://hsckzy.vip'
-                },
-                yutu: {
-                    api: 'https://yutuzy10.com',
-                    name: '玉兔资源',
-                    adult: true
-                },
-                subo: {
-                    api: 'https://subocaiji.com/api.php/provide/vod',
-                    name: '速播资源'
-                },
-                fczy: {
-                    api: 'https://api.fczy888.me/api.php/provide/vod',
-                    name: '蜂巢资源'
-                },
-                ukzy: {
-                    api: 'https://api.ukuapi88.com/api.php/provide/vod',
-                    name: 'U酷资源'
-                }
-            }
-            return true
-        } else if (password == "666666") {
-            return true
-        }
         const correctHash = window.__ENV__?.PASSWORD;
         if (!correctHash) return false;
 
@@ -133,7 +73,7 @@ function isPasswordVerified() {
         const stored = localStorage.getItem(PASSWORD_CONFIG.localStorageKey);
         if (!stored) return false;
 
-        const {timestamp, passwordHash} = JSON.parse(stored);
+        const { timestamp, passwordHash } = JSON.parse(stored);
         const currentHash = window.__ENV__?.PASSWORD;
 
         return timestamp && passwordHash === currentHash &&
